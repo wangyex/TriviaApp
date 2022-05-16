f1731f1ce42e3960fa2078ad71c9c2a4f3411a90
Ye Wang <zxcsef@msn.com>
2022-05-16 17:27:37 -0400

    Updating next button to cycle through questions

--- app/src/main/java/com/example/trivia/MainActivity.java
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -1,25 +1,66 @@
 package com.example.trivia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.example.trivia.data.AnswerListAsyncResponse;
 import com.example.trivia.data.Repository;
+import com.example.trivia.databinding.ActivityMainBinding;
 import com.example.trivia.model.Question;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+
+    private ActivityMainBinding binding;
+    private int currentQuestionIndex = 0;
+    List<Question> questionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        List<Question> questions = new Repository().getQuestions(questionArrayList -> Log.d("Main","onCreate: " + questionArrayList));
 
+        questionList = new Repository().getQuestions(questionArrayList ->{
+                    binding.questionTextView.setText(questionArrayList.get(currentQuestionIndex).getAnswer());
+                    updateCounter(questionArrayList);
+
+                }
+
+
+
+                );
+
+        binding.buttonNext.setOnClickListener(view -> {
+            currentQuestionIndex = (currentQuestionIndex + 1) % questionList.size();
+            updateQuestion();
+                });
+
+        binding.buttonFalse.setOnClickListener(view -> {
+
+
+        });
+
+        binding.buttonTrue.setOnClickListener(view -> {
+
+        });
     }
+
+    private void updateCounter(ArrayList<Question> questionArrayList) {
+        binding.textViewQuestionCounter.setText("Question: " + currentQuestionIndex+"/" + questionArrayList.size());
+    }
+
+    private void updateQuestion() {
+        String question = questionList.get(currentQuestionIndex).getAnswer();
+        binding.questionTextView.setText(question);
+    }
+
+
 }
