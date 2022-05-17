3c85f75bf5e770070c83616aa2153aac4df1cff8
Ye Wang <zxcsef@msn.com>
2022-05-17 11:10:28 -0400

    High Score and Reset Game functions

--- app/src/main/java/com/example/trivia/MainActivity.java
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -3,23 +3,32 @@ package com.example.trivia;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.example.trivia.data.AnswerListAsyncResponse;
 import com.example.trivia.data.Repository;
 import com.example.trivia.databinding.ActivityMainBinding;
 import com.example.trivia.model.Question;
+import com.example.trivia.model.Score;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity  {
 
     private ActivityMainBinding binding;
-    private int currentQuestionIndex = 0;
+    private int currentQuestionIndex = 1;
+    private  int scoreCounter = 0;
+    private Score score;
     List<Question> questionList;
 
     @Override
@@ -27,7 +36,7 @@ public class MainActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-
+        score = new Score();
 
         questionList = new Repository().getQuestions(questionArrayList ->{
                     binding.questionTextView.setText(questionArrayList.get(currentQuestionIndex).getAnswer());
@@ -44,6 +53,26 @@ public class MainActivity extends AppCompatActivity  {
             updateQuestion();
             updateCounter((ArrayList<Question>) questionList);
                 });
+
+
+
+        binding.buttonRandom.setOnClickListener(view -> {
+            //currentQuestionIndex = (currentQuestionIndex + 1) % questionList.size();
+            randomQuestion();
+            updateQuestion();
+            updateCounter((ArrayList<Question>) questionList);
+        });
+
+        binding.buttonRst.setOnClickListener(view -> {
+            //currentQuestionIndex = (currentQuestionIndex + 1) % questionList.size();
+            randomQuestion();
+            updateQuestion();
+            scoreCounter = 0;
+            score.setScore(scoreCounter);
+            binding.hiScoreText.setText(String.valueOf(score.getScore()));
+
+            updateCounter((ArrayList<Question>) questionList);
+        });
 
         binding.buttonFalse.setOnClickListener(view -> {
             checkAnswer(true);
@@ -63,9 +92,16 @@ public class MainActivity extends AppCompatActivity  {
         int snackMessageId = 0;
         if (userChoseCorrect == answer) {
             snackMessageId = R.string.correct_answer;
-
+            addPoints();
+            updateQuestion();
+            currentQuestionIndex = (currentQuestionIndex + 1) % questionList.size();
+            updateCounter((ArrayList<Question>) questionList);
         }else{
             snackMessageId = R.string.incorrect;
+            updateQuestion();
+            deductPoints();
+            shakeAnimation();
+            shakeItBaby();
         }
 
         Snackbar.make(binding.cardView, snackMessageId, Snackbar.LENGTH_SHORT).show();
@@ -73,7 +109,7 @@ public class MainActivity extends AppCompatActivity  {
     }
 
     private void updateCounter(ArrayList<Question> questionArrayList) {
-        binding.textViewQuestionCounter.setText("Question: " + currentQuestionIndex+"/" + questionArrayList.size());
+        binding.textViewQuestionCounter.setText("Question: " + currentQuestionIndex +"/" + questionArrayList.size());
     }
 
     private void updateQuestion() {
@@ -81,5 +117,42 @@ public class MainActivity extends AppCompatActivity  {
         binding.questionTextView.setText(question);
     }
 
+    private void randomQuestion(){
+        currentQuestionIndex = new Random().nextInt(questionList.size());
 
+    }
+
+    private void shakeAnimation() {
+        Animation shake = AnimationUtils.loadAnimation(MainActivity.this,R.anim.shake_animation);
+        binding.cardView.setAnimation(shake);
+
+    }
+    public void shakeItBaby() {
+        int DURATION = 250; // you can change this according to your need
+        if (Build.VERSION.SDK_INT >= 26) {
+            ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(VibrationEffect.createOneShot(DURATION, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(DURATION);
+        }
+    }
+
+    private void addPoints(){
+        scoreCounter += 100;
+        score.setScore(scoreCounter);
+        binding.hiScoreText.setText(String.valueOf(score.getScore()));
+
+    }
+    private void deductPoints(){
+
+        if (scoreCounter > 0){
+            scoreCounter -= 100;
+            score.setScore(scoreCounter);
+            binding.hiScoreText.setText(String.valueOf(score.getScore()));
+        }else {
+            scoreCounter = 0;
+            score.setScore(scoreCounter);
+
+        }
+
+    }
 }
